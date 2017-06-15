3c7c67aa1789fbd1b0fa7e887c6f2af666f823f4
lotus <wuhb@landicorp.com>
2017-06-15 16:46:23 +0800

    1.出错后，屏蔽框架信息

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/astaxie/beego"
 
 	_ "cms/src/routers"
@@ -10,5 +12,15 @@ import (
 func main() {
 	beego.SetLevel(beego.LevelDebug)
 	beego.SetLogger("console", "")
+	beego.ErrorHandler("404", page_not_found)
+	beego.ErrorHandler("401", page_note_permission)
 	beego.Run()
+}
+
+func page_not_found(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("page not found"))
+}
+
+func page_note_permission(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("permission denied"))
 }
